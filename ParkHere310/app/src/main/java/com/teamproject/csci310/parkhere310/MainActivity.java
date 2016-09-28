6fb15fa4f718151cf88a992015d8a5ca8186d6b6
Sean Yuan <seanyuan@Seans-MacBook-Pro.local>
2016-09-27 21:34:06 -0700

    Added full login screen

--- ParkHere310/app/src/main/java/com/teamproject/csci310/parkhere310/MainActivity.java
+++ ParkHere310/app/src/main/java/com/teamproject/csci310/parkhere310/MainActivity.java
@@ -2,12 +2,45 @@ package com.teamproject.csci310.parkhere310;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Button b1,b2;
+    EditText ed1,ed2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        b1=(Button)findViewById(R.id.loginButton);
+        ed1=(EditText)findViewById(R.id.emailEditText);
+        ed2=(EditText)findViewById(R.id.passwordEditText);
+
+        b2=(Button)findViewById(R.id.registerButton);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(ed1.getText().toString().equals("admin") &&
+
+                        ed2.getText().toString().equals("admin")) {
+                    Toast.makeText(getApplicationContext(), "Redirecting...",Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Toast.makeText(getApplicationContext(), "Wrong Credentials",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
