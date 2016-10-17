55f83ead23bb6534dbbcd4bfe0560f21964271c7
Curtis Zhi <rzhi@usc.edu>
2016-10-16 23:54:55 -0700

    Search operation

--- ParkHere310/app/src/main/java/com/csci310/ParkHere/SearchOperation.java
+++ ParkHere310/app/src/main/java/com/csci310/ParkHere/SearchOperation.java
@@ -1,7 +1,5 @@
 package com.csci310.ParkHere;
 
-import android.app.ProgressDialog;
-import android.os.AsyncTask;
 import android.util.Log;
 
 import org.json.JSONArray;
@@ -11,11 +9,8 @@ import org.json.JSONObject;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
-
-import javax.net.ssl.HttpsURLConnection;
 
 /**
  * Created by curtiszhi on 10/15/16.
@@ -23,28 +18,7 @@ import javax.net.ssl.HttpsURLConnection;
 
 public class SearchOperation
 {
-    private ProgressDialog dialog;
     private static final String API_KEY = "AIzaSyBxTXSEer2OE4jdVeG6AUa2UWF8QzZJhlo";
-
-    public static void parseJSON(String[] result) {
-        try {
-            JSONObject jsonObject = new JSONObject(result[0]);
-
-            double lng = ((JSONArray) jsonObject.get("results")).getJSONObject(0)
-                    .getJSONObject("geometry").getJSONObject("location")
-                    .getDouble("lng");
-
-            double lat = ((JSONArray) jsonObject.get("results")).getJSONObject(0)
-                    .getJSONObject("geometry").getJSONObject("location")
-                    .getDouble("lat");
-
-            Log.d("latitude", "" + lat);
-            Log.d("longitude", "" + lng);
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-    }
-
 
     public static String parseAddress(String address)
     {
@@ -52,7 +26,7 @@ public class SearchOperation
         String result = "";
         try
         {
-            URL url = new URL("https://maps.googleapis.com/maps/api/geocode/json?address=" + address + "&key=" + API_KEY);
+            URL url = new URL("https://maps.googleapis.com/maps/api/geocode/json?address=" + address + "&key=AIzaSyBxTXSEer2OE4jdVeG6AUa2UWF8QzZJhlo");
             URLConnection urlConnection = url.openConnection();
             String line = null;
             BufferedReader br = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
@@ -63,6 +37,27 @@ public class SearchOperation
         return result;
     }
 
+    public static String parseJSON(String result)
+    {
+        String place_id = "";
+        try
+        {
+            JSONObject jsonObject = new JSONObject(result);
+
+            double lng = ((JSONArray) jsonObject.get("results")).getJSONObject(0)
+                    .getJSONObject("geometry").getJSONObject("location")
+                    .getDouble("lng");
+
+            double lat = ((JSONArray) jsonObject.get("results")).getJSONObject(0)
+                    .getJSONObject("geometry").getJSONObject("location")
+                    .getDouble("lat");
+
+            place_id = ((JSONArray) jsonObject.get("results")).getJSONObject(0).getString("place_id");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return place_id;
+    }
     public static void search(String sTime, String eTime, String sDate, String eDate, boolean isCompact, boolean isCovered, boolean isHandicapped, String address) {
 
     }
