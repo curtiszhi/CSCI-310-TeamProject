e674a137d0b3fdcb153d36355a530b813c42a6e4
zhdllwyc <yingchew@usc.edu>
2016-10-23 15:24:32 -0700

    compile_paypal

---
+++ ParkHere310/app/src/main/java/com/csci310/ParkHere/paymentActivity.java
@@ -0,0 +1,14 @@
+package com.csci310.ParkHere;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class paymentActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment);
+    }
+
+}
