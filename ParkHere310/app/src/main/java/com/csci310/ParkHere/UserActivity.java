9a6631c3ccbb4d02067aa381287bc5cc67482ef9
Peter Lu <peterjlu@usc.edu>
2016-10-23 17:06:40 -0700

    Get info from database for User Profile

--- ParkHere310/app/src/main/java/com/csci310/ParkHere/UserActivity.java
+++ ParkHere310/app/src/main/java/com/csci310/ParkHere/UserActivity.java
@@ -15,8 +15,12 @@ import android.net.Uri;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 
 public class UserActivity extends AppCompatActivity {
 
@@ -44,18 +48,39 @@ public class UserActivity extends AppCompatActivity {
         //Pull User Info
         mFirebaseAuth = FirebaseAuth.getInstance();
         mFirebaseUser = mFirebaseAuth.getCurrentUser();
-        String userID = mFirebaseUser.getUid();
-
         mDatabase = FirebaseDatabase.getInstance().getReference();
-        String name = mDatabase.child("users").child(userID).toString();
-
+        //DatabaseReference db = FirebaseDatabase.getInstance().getReference("users");
+        DatabaseReference userRef = mDatabase.child("users/" + mFirebaseUser.getUid() + "/userName/");
+        DatabaseReference phoneRef = mDatabase.child("users/" + mFirebaseUser.getUid() + "/phone/");
+        String name = userRef.getKey();
         String email = mFirebaseUser.getEmail();
+        String phone = phoneRef.getKey();
         Uri uri = mFirebaseUser.getPhotoUrl();
 
+        // Doesn't work
+        /*
+        db.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                User user = dataSnapshot.getValue(User.class);
+                nameEditText.setText(user.userName, TextView.BufferType.EDITABLE);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+        */
+
+        //Set User Info
         nameEditText.setText(name,TextView.BufferType.EDITABLE);
         emailEditText.setText(email,TextView.BufferType.EDITABLE);
+        phoneEditText.setText(phone,TextView.BufferType.EDITABLE);
         profilePicImageView.setImageURI(uri);
 
+
+        //Set EditText Not Editable
         nameEditText.setTag(nameEditText.getKeyListener());
         nameEditText.setKeyListener(null);
 
