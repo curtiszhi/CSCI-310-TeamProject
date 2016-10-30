65c258e120789468f46b53ebe811d88e59d41690
Peter Lu <peterjlu@usc.edu>
2016-10-29 18:55:20 -0700

    Fix InputType for EditTexts

--- ParkHere310/app/src/main/java/com/csci310/ParkHere/ActionActivity.java
+++ ParkHere310/app/src/main/java/com/csci310/ParkHere/ActionActivity.java
@@ -27,7 +27,6 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 /**
  * Created by seanyuan on 9/28/16.
@@ -97,7 +96,7 @@ public class ActionActivity extends AppCompatActivity {
         database.orderByChild("email").equalTo(mFirebaseUser_universal.getEmail()).addChildEventListener(new ChildEventListener() {
             @Override
             public void onChildAdded(DataSnapshot dataSnapshot, String prevChildKey) {
-                //user_all = dataSnapshot.getValue(User.class);
+                user_all = dataSnapshot.getValue(User.class);
             }
 
             @Override
