086970242490871719f6af5bd760676f47bb001d
Peter Lu <peterjlu@usc.edu>
2016-10-29 18:28:22 -0700

    Fixed getting info for user profile.

--- ParkHere310/app/src/main/java/com/csci310/ParkHere/UserActivity.java
+++ ParkHere310/app/src/main/java/com/csci310/ParkHere/UserActivity.java
@@ -1,26 +1,26 @@
 package com.csci310.ParkHere;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
 import android.text.method.KeyListener;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ToggleButton;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.net.Uri;
+import android.widget.ToggleButton;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserProfileChangeRequest;
-import com.google.firebase.database.ChildEventListener;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.io.IOException;
 
 public class UserActivity extends AppCompatActivity {
 
@@ -31,6 +31,7 @@ public class UserActivity extends AppCompatActivity {
     private FirebaseAuth mFirebaseAuth;
     private static FirebaseUser mFirebaseUser;
     private DatabaseReference mDatabase;
+    private final static int SELECT_PHOTO = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +46,17 @@ public class UserActivity extends AppCompatActivity {
         viewRentHistoryButton = (Button) findViewById(R.id.viewRentHistoryButton);
         returnHomeScreenButton = (Button) findViewById(R.id.returnHomeScreenButton);
 
+        // Edit Profile Pic (Not Tested Yet)
+        profilePicImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                Intent pickPhotoIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                pickPhotoIntent.setType("image/*");
+                startActivityForResult(Intent.createChooser(pickPhotoIntent,"Select Picture"), SELECT_PHOTO);
+            }
+        });
+
+
         //Get User Info
         mFirebaseAuth = FirebaseAuth.getInstance();
         mFirebaseUser = mFirebaseAuth.getCurrentUser();
@@ -54,21 +66,6 @@ public class UserActivity extends AppCompatActivity {
         String name = ActionActivity.user_all.getUserName();
         Uri uri = mFirebaseUser.getPhotoUrl();
 
-        // Doesn't work atm
-/*
-        userRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                String name = dataSnapshot.getValue(String.class);
-                nameEditText.setText(name, TextView.BufferType.EDITABLE);
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-
-            }
-        });
-*/
 
         //Set User Info
         nameEditText.setText(name.trim(),TextView.BufferType.EDITABLE);
@@ -135,5 +132,22 @@ public class UserActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+
+    //Update Profile Pic (Not Tested Yet)
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == SELECT_PHOTO && resultCode == RESULT_OK && data != null && data.getData() != null){
+            Uri uri = data.getData();
+            try{
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+
+                profilePicImageView.setImageBitmap(bitmap);
+            } catch(IOException e){
+                e.printStackTrace();
+            }
+        }
     }
 }
