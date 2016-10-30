e2b5518b7882e5abb1c5755ae77e8af6aaeb3df5
Sean Yuan <seanyuan@usc.edu>
2016-10-30 14:44:24 -0700

    update rating

--- ParkHere310/app/src/main/java/com/csci310/ParkHere/RatingActivity.java
+++ ParkHere310/app/src/main/java/com/csci310/ParkHere/RatingActivity.java
@@ -2,11 +2,20 @@ package com.csci310.ParkHere;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RatingBar;
 import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class RatingActivity extends AppCompatActivity {
     private String host_name;
@@ -18,11 +27,20 @@ public class RatingActivity extends AppCompatActivity {
     private EditText commentHostText, commentSpotText;
     private RatingBar userRateHost,userRateSpot;
     private Button rate;
+    private static DatabaseReference mDatabase;
+    private FirebaseAuth mFirebaseAuth;
+    private static FirebaseUser mFirebaseUser;
+    FeedItem fd;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rating);
-        getInfo();
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mFirebaseUser = mFirebaseAuth.getCurrentUser();
+        Bundle bundle = getIntent().getExtras();
+        String value = bundle.getString("parkingID");
+        getInfo(value);
         hostText= (TextView) findViewById(R.id.host_name);
         hostText.setText(host_name);
         userRateHost = (RatingBar) findViewById(R.id.RateHost);
@@ -38,16 +56,40 @@ public class RatingActivity extends AppCompatActivity {
                 rateSpot=userRateSpot.getRating();
                 commentHost=commentHostText.getText().toString();
                 commentSpot=commentSpotText.getText().toString();
-                update();
+                update(fd);
 
             }
         });
 
     }
-    private void getInfo(){
+    private void getInfo(String parkID){
+        mDatabase.child("parking-spots-renting");
+        mDatabase.orderByChild("identifier").equalTo(parkID).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot child : dataSnapshot.getChildren()) {
+                    Log.d("User key", child.getKey());
+                    Log.d("User ref", child.getRef().toString());
+                    Log.d("User val", child.getValue().toString());
+                    fd = (FeedItem) child.getValue();
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
 
     }
-    private void update(){
+    private void update(FeedItem feeder){
+        mDatabase.child("parking-spots-hosting").child(feeder.getIdentifier()).child("rating").setValue(rateSpot);
+        //needs to be calculated, right now it is overwriting
+        mDatabase.child("parking-spots-hosting").child(feeder.getIdentifier()).child("review").child(mFirebaseUser.getUid()).setValue(commentSpot);
 
+
+        mDatabase.child("users/"+feeder.getHost()+"/hosting").child(feeder.getIdentifier()).child("rating").setValue(rateHost);
+        //needs to be calculated, right now it is overwriting
+        mDatabase.child("users/"+feeder.getHost()+"/hosting").child(feeder.getIdentifier()).child("review").setValue(commentHost);
     }
 }
