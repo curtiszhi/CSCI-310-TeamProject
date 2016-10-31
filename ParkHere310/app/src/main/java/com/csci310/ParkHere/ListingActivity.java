fd98ef649da6055d6f7b84529e177554a7378f2e
zhdllwyc <yingchew@usc.edu>
2016-10-31 16:16:10 -0700

    update_detail

--- ParkHere310/app/src/main/java/com/csci310/ParkHere/ListingActivity.java
+++ ParkHere310/app/src/main/java/com/csci310/ParkHere/ListingActivity.java
@@ -213,7 +213,7 @@ public class ListingActivity extends AppCompatActivity {
                         user_all.setEndTime((String)value);
                     }
                     if(key.equals("price")){
-                        user_all.setPrice((Double)value);
+                        user_all.setPrice((double)value);
                     }
                     if(key.equals("cancel")){
                         user_all.setCancel((String)value);
