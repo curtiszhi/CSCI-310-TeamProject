57a86ae09f27167acd3ef6e709aa46856ab7dffb
Curtis Zhi <rzhi@usc.edu>
2016-10-31 22:50:39 -0700

    Fixed bugs

--- ParkHere310/app/src/main/java/com/csci310/ParkHere/RentActivity.java
+++ ParkHere310/app/src/main/java/com/csci310/ParkHere/RentActivity.java
@@ -354,11 +354,13 @@ public class RentActivity extends AppCompatActivity {
         String time_frame=start+" to "+end;
         time.setText(time_frame);
         String filter_spot="";
-        for(int i=0;i<fd.getFilter().size();i++){
-            if(i!=fd.getFilter().size()-1){
-            filter_spot=filter_spot+fd.getFilter().get(i)+", ";}
-            else{
-                filter_spot=filter_spot+fd.getFilter().get(i);
+        if (fd.getFilter() != null) {
+            for (int i = 0; i < fd.getFilter().size(); i++) {
+                if (i != fd.getFilter().size() - 1) {
+                    filter_spot = filter_spot + fd.getFilter().get(i) + ", ";
+                } else {
+                    filter_spot = filter_spot + fd.getFilter().get(i);
+                }
             }
         }
         filters.setText(filter_spot);
@@ -394,10 +396,12 @@ public class RentActivity extends AppCompatActivity {
     }
 
     public void display(){
-        byte[] decodedString = Base64.decode(spotPhoto.get(0), Base64.DEFAULT);
-        Bitmap b = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
-        image_view.setImageBitmap(b);
-        image_label.setText("1 of "+spotPhoto.size()+" images");
+        if (spotPhoto.size() > 0) {
+            byte[] decodedString = Base64.decode(spotPhoto.get(0), Base64.DEFAULT);
+            Bitmap b = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
+            image_view.setImageBitmap(b);
+            image_label.setText("1 of " + spotPhoto.size() + " images");
+        }
     }
 
 
