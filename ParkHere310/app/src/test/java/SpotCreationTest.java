39e2e839809905658f8edee775b6fbdc624f49dd
zhdllwyc <yingchew@usc.edu>
2016-11-06 14:17:59 -0800

    feeditem

--- ParkHere310/app/src/test/java/SpotCreationTest.java
+++ ParkHere310/app/src/test/java/SpotCreationTest.java
@@ -9,7 +9,9 @@ import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Vector;
 
 import static org.hamcrest.CoreMatchers.is;
@@ -42,44 +44,64 @@ public class SpotCreationTest {
         spot.setCurrentRenter("ABCDEFG");//rent page didn't add current
         spot.setSpotID("23456");
         spot.setStartDates("11-10-2016");
-        spot
+        spot.setStartTime("13:10PM");
+        spot.setEndDates("11-20-2016");
+        spot.setEndTime("10:30AM");
+        spot.setIdentifier("asdfghjklkjhgf");
+        spot.setPrice(2.3);
 
-        assertThat(email, is("testemail@gmail.com"));
-        Boolean isHost = test_user.getHost();
-        assertThat(isHost, is(true));
-        String phone = test_user.getPhone();
-        assertThat(phone, is("2222222222"));
-        String username = test_user.getUserName();
-        assertThat(username, is("Billy Test"));
+        assertThat(spot.getAddress(), is("1171 W30th Street,Los Angeles,CA90007"));
+        assertThat(spot.getHost(), is("abcdefg"));
+        assertThat(spot.getFilter(), is(filters));
+        assertThat(spot.getDescription(), is("test description"));
+        assertThat(spot.getActivity(), is(true));
+        assertThat(spot.getCancel(), is("No refund"));
+        assertThat(spot.getCurrentRenter(), is("ABCDEFG"));
+        assertThat(spot.getSpotID(), is("23456"));
+        assertThat(spot.getStartDates(), is("11-10-2016"));
+        assertThat(spot.getStartTime(), is("13:10PM"));
+        assertThat(spot.getEndDates(), is("11-20-2016"));
+        assertThat(spot.getEndTime(), is("10:30AM"));
+        assertThat(spot.getIdentifier(), is("asdfghjklkjhgf"));
+        assertThat(spot.getPrice(), is(2.3));
+
     }
 
     @Test
     public void UserAdvancedFunctions(){
-        User test_user = new User();
-        assertThat(test_user.getReview(), is(notNullValue()));
-        assertThat(test_user.getRateList(), is(notNullValue()));
-        assertThat(test_user.getRating(), is(notNullValue()));
-        assertThat(test_user.getRenting(), is(notNullValue()));
-        assertThat(test_user.getHosting(), is(notNullValue()));
-        assertThat(test_user.getPhoto(), is(nullValue()));
+        FeedItem spot=new FeedItem();
+        assertThat(spot.getReview(), is(notNullValue()));
+        assertThat(spot.getRentedTime(), is(notNullValue()));
+        assertThat(spot.getRating(), is(notNullValue()));
+        assertThat(spot.getLatitude(), is(0.0));
+        assertThat(spot.getLongitude(), is(0.0));
+        assertThat(spot.getPhotos(), is(notNullValue()));
 
         Vector<String> testReviews = new Vector<String>();
         testReviews.add("Test review1");
         testReviews.add("Test review2");
         testReviews.add("Test review3");
         testReviews.add(" ");
-        test_user.setReview(testReviews);
-        assertThat(test_user.getReview().size(), is(4));
-        assertThat(test_user.getReview().toString(), is("[Test review1, Test review2, Test review3,  ]"));
+        spot.setReview(testReviews);
+        assertThat(spot.getReview().size(), is(4));
+        assertThat(spot.getReview().toString(), is("[Test review1, Test review2, Test review3,  ]"));
 
         Vector<Integer> testRatings = new Vector<Integer>();
         testRatings.add(3);
         testRatings.add(5);
         testRatings.add(1);
         testRatings.add(2);
-        test_user.setRating(testRatings);
-        assertThat(test_user.getRating(), is((float)2.75));
+        spot.setRating(testRatings);
+        assertThat(spot.calculateRate(), is((float)2.75));
 
+        Map<String,Vector<String>> testRented=new HashMap<String,Vector<String>>();
+        Vector<String> rented = new Vector<String>();
+        rented.add("11-10-2016 13:10PM");
+        rented.add("11-20-2016 10:30AM");
+        testRented.put("234567",rented);
+        spot.setRentedTime(testRented);
+        assertThat(spot.getRentedTime().size(), is(1));
+        assertThat(spot.getRentedTime(), is(testRented));
 
     }
 }
