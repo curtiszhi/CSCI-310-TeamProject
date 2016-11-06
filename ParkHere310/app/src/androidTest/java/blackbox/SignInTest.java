d82464c7af3dfe25f3038d5aa2ecacc4e48cc32e
Peter Lu <peterjlu@usc.edu>
2016-11-06 15:56:09 -0800

    Clean up Sign in Test

--- ParkHere310/app/src/androidTest/java/blackbox/SignInTest.java
+++ ParkHere310/app/src/androidTest/java/blackbox/SignInTest.java
@@ -1,49 +1,65 @@
 package blackbox;
 
-
-import android.support.test.espresso.ViewInteraction;
-import android.support.test.espresso.matcher.ViewMatchers;
+import android.support.test.espresso.NoMatchingViewException;
 import android.support.test.rule.ActivityTestRule;
-import android.support.test.runner.AndroidJUnit4;
-import android.test.suitebuilder.annotation.LargeTest;
 
+import com.csci310.ParkHere.ActionActivity;
 import com.csci310.ParkHere.MainActivity;
 import com.csci310.ParkHere.R;
 
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
-import static android.support.test.espresso.action.ViewActions.replaceText;
-import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
-import static org.hamcrest.Matchers.allOf;
 
-@RunWith(AndroidJUnit4.class)
+/**
+ * Created by Peter on 11/3/16.
+ */
+
+
+@RunWith(JUnit4.class)
 public class SignInTest {
+    private String email, pass;
 
     @Rule
-    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+    public ActivityTestRule<MainActivity> myActivityRule = new ActivityTestRule<>(
+            MainActivity.class);
+
+    @Before
+    public void initString() {
+        email = "peter@gmail.com";
+        pass = "Password1";
+    }
+
 
     @Test
-    public void signInTest() {
+    public void signIn()
+    {
+        // Enter Sign In Info
 
-        ViewInteraction appCompatEditText2 = onView(
-                allOf(ViewMatchers.withId(R.id.emailEditText), isDisplayed()));
-        appCompatEditText2.perform(replaceText("peter@gmail.com"), closeSoftKeyboard());
-
-        ViewInteraction appCompatEditText3 = onView(
-                allOf(withId(R.id.passwordEditText), isDisplayed()));
-        appCompatEditText3.perform(replaceText("Password1"), closeSoftKeyboard());
-
-        ViewInteraction appCompatButton = onView(
-                allOf(withId(R.id.loginButton), withText("login"), isDisplayed()));
-        appCompatButton.perform(click());
-
+        try {
+            onView(withId(R.id.emailEditText)).perform(typeText(email), closeSoftKeyboard());
+            onView(withId(R.id.passwordEditText)).perform(typeText(pass), closeSoftKeyboard());
+            onView(withId(R.id.loginButton)).perform(click());
+            intended(hasComponent(ActionActivity.class.getName()));
+        }
+        catch (NoMatchingViewException e)
+        {
+            onView(withId(R.id.locationEditText)).perform(typeText(email), closeSoftKeyboard());
+            onView((withId(R.id.locationEditText))).check(matches(withText(email)));
+        }
     }
+
 
 }
