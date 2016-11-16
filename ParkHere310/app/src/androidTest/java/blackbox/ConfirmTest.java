c67fbe237595b949d0bad8f77b32fa0f141603cf
Curtis Zhi <rzhi@usc.edu>
2016-11-15 23:22:05 -0800

    Split booking done

---
+++ ParkHere310/app/src/androidTest/java/blackbox/ConfirmTest.java
@@ -0,0 +1,99 @@
+package blackbox;
+
+import android.support.test.rule.ActivityTestRule;
+
+import com.csci310.ParkHere.RegisterActivity;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import android.support.test.espresso.UiController;
+import android.support.test.espresso.ViewAction;
+import android.support.test.espresso.action.GeneralClickAction;
+import android.support.test.espresso.action.GeneralLocation;
+import android.support.test.espresso.action.Press;
+import android.support.test.espresso.action.Tap;
+import android.support.test.espresso.contrib.PickerActions;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TimePicker;
+
+import com.csci310.ParkHere.AddActivity;
+import com.csci310.ParkHere.R;
+
+import org.hamcrest.Matcher;
+import org.hamcrest.Matchers;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.InstrumentationRegistry.getInstrumentation;
+import static android.support.test.espresso.Espresso.onData;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.scrollTo;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.doesNotExist;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.isEnabled;
+import static android.support.test.espresso.matcher.ViewMatchers.isClickable;
+import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
+import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.anything;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasEntry;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.is;
+
+/**
+ * Created by curtiszhi on 11/14/16.
+ */
+
+@RunWith(JUnit4.class)
+public class ConfirmTest {
+
+    private String name, email, pass, phone, existingEmail;
+
+    @Rule
+    public ActivityTestRule<RegisterActivity> myActivityRule = new ActivityTestRule<>(
+            RegisterActivity.class);
+
+    @Before
+    public void initString()
+    {
+        try{ Thread.sleep(2000); }catch (Exception _){}
+        try
+        {
+            onView(withText("OK")).perform(click());
+        }
+        catch (Exception e)
+        {
+
+        }
+
+
+
+    }
+
+
+    @Test
+    public void registerUser()
+    {
+
+    }
+}
---
+++ ParkHere310/app/src/main/java/com/csci310/ParkHere/SplitParkingSpot.java
@@ -0,0 +1,103 @@
+package com.csci310.ParkHere;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.text.ParseException;
+import java.util.ArrayList;
+
+/**
+ * Created by curtiszhi on 11/15/16.
+ */
+
+public class SplitParkingSpot
+{
+    private FeedItem originalSpot;
+    private String userStart;
+    private String userEnd;
+    private String originalStart;
+    private String originalEnd;
+    private boolean timeElapsedBefore;
+    private boolean timeElapsedAfter;
+    private java.text.SimpleDateFormat sdf;
+
+    SplitParkingSpot(FeedItem fd, String userStart, String userEnd)
+    {
+        //Initialize properties:
+        originalSpot = fd;
+        if (userStart.contains("M"))
+            this.userStart = userStart.substring(0, userStart.length() - 2);
+        else
+            this.userStart = userStart;
+        if (userStart.contains("M"))
+            this.userEnd = userStart.substring(0, userEnd.length() - 2);
+        else
+            this.userEnd = userEnd;
+        originalStart = originalSpot.getStartDates() + " " + originalSpot.getStartTime().substring(0, originalSpot.getStartTime().length() - 2);
+        originalEnd = originalSpot.getEndDates() + " " + originalSpot.getEndTime().substring(0, originalSpot.getEndTime().length() - 2);
+        sdf = new java.text.SimpleDateFormat("MM-dd-yyyy HH:mm");
+        try {
+            timeElapsedBefore = sdf.parse(this.userStart).getTime() > sdf.parse(originalStart).getTime();
+            timeElapsedAfter = sdf.parse(originalEnd).getTime() > sdf.parse(this.userEnd).getTime();
+        }
+        catch (ParseException e)
+        {
+            e.printStackTrace();
+        }
+
+        //Split the spot and update the database:
+        for (FeedItem spot : getUpdatedSpots())
+            addSpot(spot);
+    }
+
+    private ArrayList<FeedItem> getUpdatedSpots()
+    {
+        ArrayList<FeedItem> spots = new ArrayList<>();
+
+        spots.add(setSpotWithTimeActivity(userStart, userEnd, false));
+        if (timeElapsedBefore)
+            spots.add(setSpotWithTimeActivity(originalStart, userStart, true));
+        if (timeElapsedAfter)
+            spots.add(setSpotWithTimeActivity(userEnd, originalEnd, true));
+
+        return spots;
+    }
+
+    private FeedItem setSpotWithTimeActivity(String start, String end, boolean activity)
+    {
+        FeedItem fd = (FeedItem)SplitParkingSpot.deepClone(originalSpot);
+        if (fd != null)
+        {
+            fd.setStartDates(start.split(" ")[0]);
+            fd.setStartTime(start.split(" ")[1]);
+            fd.setEndDates(end.split(" ")[0]);
+            fd.setEndTime(end.split(" ")[1]);
+            fd.setActivity(activity);
+        }
+
+        return fd;
+    }
+
+    private void addSpot(FeedItem spot)
+    {
+
+    }
+
+    public static Object deepClone(Object object)
+    {
+        try
+        {
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            ObjectOutputStream oos = new ObjectOutputStream(baos);
+            oos.writeObject(object);
+            ByteArrayInputStream bais = new ByteArrayInputStream(baos.toByteArray());
+            ObjectInputStream ois = new ObjectInputStream(bais);
+            return ois.readObject();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
